c8e920ce14210853aeef24c1bb9468f8dae11e5f
cpt-cope <copeandsongaming@gmail.com>
2026-03-31 23:18:39 -0500

    3: 3rd commit

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
+from shot import Shot
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
-
+from constants import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.x = x
         self.y = y
         self.rotation = 0
+        self.shot_cd_timer = 0
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
     
@@ -37,8 +38,20 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+        if keys[pygame.K_SPACE]:
+            if self.shot_cd_timer > 0:
+                print("can't shoot")
+            else:
+                self.shot_cd_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+                self.shoot()
+
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
