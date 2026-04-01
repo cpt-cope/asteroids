7bd92510fcce158cb735e7a94d27bc787c7bb864
cpt-cope <copeandsongaming@gmail.com>
2026-03-31 20:24:32 -0500

    2: This is the second update

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+
+  
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill(black)
-    pygame.display.flip()
+        updatable.update(dt)
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip
+        dt = (clock.tick(60) / 1000)
+
+        pygame.display.flip()
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
