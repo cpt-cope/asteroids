a9d9575bbc182b98780534dd9d4d5c6a6a813744
cpt-cope <copeandsongaming@gmail.com>
2026-03-31 23:19:32 -0500

    4:

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt   
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_number = random.uniform(20, 50)
+        asteroid1_direction = self.velocity.rotate(random_number)
+        asteroid2_direction = self.velocity.rotate(-random_number)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = asteroid1_direction * 1.2
+        asteroid2.velocity = asteroid2_direction * 1.2
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import sys
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-
+from logger import log_event
+from shot import Shot
 
 
 def main():
@@ -15,6 +17,8 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, drawable, updatable)
     AsteroidField.containers = (updatable)
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
@@ -28,6 +32,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt)
+        player.shot_cd_timer -= dt
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over")
+                sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event(("asteroid_shot"))
+                    shot.kill()
+                    asteroid.split()
+
+
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
